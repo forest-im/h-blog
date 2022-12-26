c10874ef01c4c4f8ab365e874972d93b22aa2258
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-12-27 01:15:51 +0900

    Feat custom document 테스트

--- pages/_document.js
+++ pages/_document.js
@@ -1,47 +1,107 @@
 import React from "react";
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+// import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
-        {/* <link rel="stylesheet" href="github-markdown.css" /> */}
-        {/* <link href="styles/markdown.css" rel="stylesheet"></link> */}
-        {/* <link rel="stylesheet" href="github-markdown.css" /> */}
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown-light.min.css"
-          integrity="sha512-zb2pp+R+czM7GAemdSUQt6jFmr3qCo6ikvBgVU6F5GvwEDR0C2sefFiPEJ9QUpmAKdD5EqDUdNRtbOYnbF/eyQ=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
-        <style>
-          {`	.markdown-body {
-            box-sizing: border-box;
-            min-width: 200px;
-            max-width: 980px;
-            margin: 0 auto;
-            padding: 45px;
-          }
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const originalRenderPage = ctx.renderPage;
 
-          .markdown-body pre{
-            border-radius: 10px;
-            border: 1px solid gray;
-            background-color: rgb(253, 253, 253);
-          }
+    // Run the React rendering logic synchronously
+    ctx.renderPage = () =>
+      originalRenderPage({
+        // Useful for wrapping the whole react tree
+        enhanceApp: (App) => App,
+        // Useful for wrapping in a per-page basis
+        enhanceComponent: (Component) => Component,
+      });
 
-          @media (max-width: 767px) {
-            .markdown-body {
-              padding: 15px;
+    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown-light.min.css"
+            integrity="sha512-zb2pp+R+czM7GAemdSUQt6jFmr3qCo6ikvBgVU6F5GvwEDR0C2sefFiPEJ9QUpmAKdD5EqDUdNRtbOYnbF/eyQ=="
+            crossOrigin="anonymous"
+            referrerPolicy="no-referrer"
+          />
+          <style>
+            {`	.markdown-body {
+              box-sizing: border-box;
+              min-width: 200px;
+              max-width: 980px;
+              margin: 0 auto;
+              padding: 45px;
             }
-          }`}
-        </style>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+
+            .markdown-body pre{
+              border-radius: 10px;
+              border: 1px solid gray;
+              background-color: rgb(253, 253, 253);
+            }
+
+            @media (max-width: 767px) {
+              .markdown-body {
+                padding: 15px;
+              }
+            }`}
+          </style>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
+
+// export default function Document() {
+//   return (
+//     <Html>
+//       <Head>
+//         <link
+//           rel="stylesheet"
+//           href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown-light.min.css"
+//           integrity="sha512-zb2pp+R+czM7GAemdSUQt6jFmr3qCo6ikvBgVU6F5GvwEDR0C2sefFiPEJ9QUpmAKdD5EqDUdNRtbOYnbF/eyQ=="
+//           crossOrigin="anonymous"
+//           referrerPolicy="no-referrer"
+//         />
+//         <style>
+//           {`	.markdown-body {
+//             box-sizing: border-box;
+//             min-width: 200px;
+//             max-width: 980px;
+//             margin: 0 auto;
+//             padding: 45px;
+//           }
+
+//           .markdown-body pre{
+//             border-radius: 10px;
+//             border: 1px solid gray;
+//             background-color: rgb(253, 253, 253);
+//           }
+
+//           @media (max-width: 767px) {
+//             .markdown-body {
+//               padding: 15px;
+//             }
+//           }`}
+//         </style>
+//       </Head>
+//       <body>
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   );
+// }
