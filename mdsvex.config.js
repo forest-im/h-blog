31e3c8d57d94bc8216f67e7ead951e2aad40fa51
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-01-30 20:23:05 +0900

    Feat tailwind 설정 추가, mdsvex, svelte 설정 정리

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -2,30 +2,18 @@
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeKatexSvelte from 'rehype-katex-svelte';
 import rehypeSlug from 'rehype-slug';
+import remarkGfm from 'remark-gfm';
 import remarkAbbr from 'remark-abbr';
 import remarkMath from 'remark-math';
-// import react-syntax-highlighter/dist/cjs/styles/prism/index.js as ;
-// import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-// import { coy } from 'react-syntax-highlighter/dist/cjs/styles/prism';
-// import { fileURLToPath } from 'url';
-// import matter from 'gray-matter';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+import remarkHtml from 'remark-html';
 
 const config = {
 	extensions: ['.md', '.svelte'],
 	smartypants: {
 		dashes: 'oldschool'
 	},
-	remarkPlugins: [remarkMath, remarkAbbr],
+	remarkPlugins: [remarkMath, remarkAbbr, remarkHtml],
 	rehypePlugins: [rehypeKatexSvelte, rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }]]
-	// layout: {
-	// 	_: path.join(__dirname, './src/components/LayoutDefault.svelte')
-	// }
-	// highlight: {
-	// 	highlighter: { alias: { coy: 'javascript' } }
-	// },
 };
 
 export default config;
