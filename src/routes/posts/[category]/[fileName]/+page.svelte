5f23040a8ef488873ad724e76c218bcb3bf85963
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-02-02 08:15:58 +0900

    Feat comment 노드 추가

--- src/routes/posts/[category]/[fileName]/+page.svelte
+++ src/routes/posts/[category]/[fileName]/+page.svelte
@@ -1,9 +1,25 @@
 <script>
 	import PageHead from "$lib/components/PageHead.svelte";
 	import PostTitle from "$lib/components/PostTitle.svelte";
+	import { onMount } from "svelte/internal";
 
 	export let data;
 	$: component = data.component;
+
+	let commentSection;
+
+	onMount(() => {
+		if (commentSection.childNodes.length) return;
+		const scriptElem = document.createElement("script");
+		scriptElem.src = "https://utteranc.es/client.js";
+		scriptElem.async = true;
+		scriptElem.setAttribute("repo", "h-alex2/h-blog");
+		scriptElem.setAttribute("issue-term", "pathname");
+		scriptElem.setAttribute("theme", "github-light");
+		scriptElem.setAttribute("label", "blog-comment");
+		scriptElem.crossOrigin = "anonymous";
+		commentSection.appendChild(scriptElem);
+	});
 </script>
 
 <article>
@@ -11,4 +27,6 @@
 	<PostTitle title={data.metadata.title} />
 
 	<svelte:component this={component} />
+
+	<section bind:this={commentSection} class="my-10" />
 </article>
