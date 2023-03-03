90f24fd0321eab3cbd0fb50dbc6734d3b22e7159
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-03-04 02:50:32 +0900

    Chore og image 삭제

--- src/lib/components/PageHead.svelte
+++ src/lib/components/PageHead.svelte
@@ -9,6 +9,5 @@
   <meta property="og:site_name" content="AlexIm" />
   <meta property="og:title" content={title} />
   <meta property="og:description" content={description} />
-  <meta property="og:image" content={image}>
   <script src="https://cdn.jsdelivr.net/npm/prismjs@1/prism.min.js"></script>
 </svelte:head>
