3053d06d2662f2bac12e47370dd4abebcce9befe
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-03-04 10:20:05 +0900

    Feat twitter card 추가

--- src/lib/components/PageHead.svelte
+++ src/lib/components/PageHead.svelte
@@ -9,5 +9,8 @@
   <meta property="og:site_name" content="AlexIm" />
   <meta property="og:title" content={title} />
   <meta property="og:description" content={description} />
+  <meta property="twitter:site" content="AlexIm" />
+  <meta property="twitter:title" content={title} />
+  <meta property="twitter:description" content={description} />
   <script src="https://cdn.jsdelivr.net/npm/prismjs@1/prism.min.js"></script>
 </svelte:head>
