6d343b8a29e46c5704eb60c43febd41c72b0ccc7
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-03-04 14:39:47 +0900

    Chore meta image 추가

--- src/lib/components/PageHead.svelte
+++ src/lib/components/PageHead.svelte
@@ -1,8 +1,8 @@
 <script>
+  import img from "$lib/images/profile02.png";
+
   export let title = "";
   export let description = "";
-  export let image = "";
-
 </script>
 
 <svelte:head>
@@ -10,10 +10,11 @@
   <meta property="og:site_name" content="AlexIm" />
   <meta property="og:title" content={title} />
   <meta property="og:description" content={description} />
-  <meta name="twitter:card" content="summary" />
-  <meta name="twitter:site" content="AlexIm" />
+  <meta property="og:image" content={img} />
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:site" content="AlexIm">
   <meta name="twitter:title" content={title} />
   <meta name="twitter:description" content={description} />
-  <meta name="twitter:image" content={image} />
+  <meta name="twitter:image" content={img} />
   <script src="https://cdn.jsdelivr.net/npm/prismjs@1/prism.min.js"></script>
 </svelte:head>
