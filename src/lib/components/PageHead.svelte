c64eb423268b35c5c20c222c17643c7534a8da1a
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-03-04 10:29:36 +0900

    Chore 트위터 카드 summary 추가

--- src/lib/components/PageHead.svelte
+++ src/lib/components/PageHead.svelte
@@ -9,6 +9,7 @@
   <meta property="og:site_name" content="AlexIm" />
   <meta property="og:title" content={title} />
   <meta property="og:description" content={description} />
+  <meta property="twitter:card" content="summary" />
   <meta property="twitter:site" content="AlexIm" />
   <meta property="twitter:title" content={title} />
   <meta property="twitter:description" content={description} />
