6b70c8d70a55bbe161ad835a4c133493548876ba
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-03-04 13:51:35 +0900

    Chore 트위터 메타태그 수정

--- src/lib/components/PageHead.svelte
+++ src/lib/components/PageHead.svelte
@@ -9,9 +9,9 @@
   <meta property="og:site_name" content="AlexIm" />
   <meta property="og:title" content={title} />
   <meta property="og:description" content={description} />
-  <meta property="twitter:card" content="summary" />
-  <meta property="twitter:site" content="AlexIm" />
-  <meta property="twitter:title" content={title} />
-  <meta property="twitter:description" content={description} />
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:site" content="AlexIm" />
+  <meta name="twitter:title" content={title} />
+  <meta name="twitter:description" content={description} />
   <script src="https://cdn.jsdelivr.net/npm/prismjs@1/prism.min.js"></script>
 </svelte:head>
