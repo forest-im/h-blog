9b4717ee0574dc6ba85da64509d10cf66cd3f8a1
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-08-12 23:42:14 +0900

    Chore about 페이지 image 추가

--- src/routes/about/photos/+page.svelte
+++ src/routes/about/photos/+page.svelte
@@ -1,21 +1,50 @@
-<script>
-	import photo01 from "$lib/images/photos/01.jpg";
-	import photo02 from "$lib/images/photos/02.jpg";
-	import photo03 from "$lib/images/photos/03.jpg";
-	import photo04 from "$lib/images/photos/04.jpg";
-	import photo05 from "$lib/images/photos/05.jpg";
-	import photo06 from "$lib/images/photos/06.jpg";
-	import photo07 from "$lib/images/photos/07.jpg";
-	import photo08 from "$lib/images/photos/08.jpg";
-</script>
-
 <div class="mb-40 flex flex-col items-center overflow-hidden">
-	<img class="w-full max-w-[50rem] hover:grayscale-0" src={photo07} alt="my-photo-07" />
-	<img class="w-full max-w-[50rem] hover:grayscale-0" src={photo01} alt="my-photo-01" />
-	<img class="w-full max-w-[50rem] hover:grayscale-0" src={photo02} alt="my-photo-02" />
-	<img class="w-full max-w-[50rem] hover:grayscale-0" src={photo03} alt="my-photo-03" />
-	<img class="w-full max-w-[50rem] hover:grayscale-0" src={photo04} alt="my-photo-04" />
-	<img class="w-full max-w-[50rem] hover:grayscale-0" src={photo05} alt="my-photo-05" />
-	<img class="w-full max-w-[50rem] hover:grayscale-0" src={photo06} alt="my-photo-06" />
-	<img class="w-full max-w-[50rem] hover:grayscale-0" src={photo08} alt="my-photo-08" />
+	<img
+		class="w-full max-w-[50rem] hover:grayscale-0"
+		src="https://github.com/h-alex2/images/assets/84281505/103fd3e5-f93b-4c05-b687-a3610b86e708"
+		alt="my-photo-07"
+		width="800"
+	/>
+	<img
+		class="w-full max-w-[50rem] hover:grayscale-0"
+		src="https://github.com/h-alex2/images/assets/84281505/8900f869-9831-43f3-813c-3710ca774923"
+		alt="my-photo-01"
+		width="800"
+	/>
+	<img
+		class="w-full max-w-[50rem] hover:grayscale-0"
+		src="https://github.com/h-alex2/images/assets/84281505/8fe2cf09-d490-41eb-bc7d-5a2f79ead085"
+		alt="my-photo-02"
+		width="800"
+	/>
+	<img
+		class="w-full max-w-[50rem] hover:grayscale-0"
+		src="https://github.com/h-alex2/images/assets/84281505/863883df-f94b-4285-8b65-9e333424dd9e"
+		alt="my-photo-03"
+		width="800"
+	/>
+	<img
+		class="w-full max-w-[50rem] hover:grayscale-0"
+		src="https://github.com/h-alex2/images/assets/84281505/6454586f-48aa-4f6c-a8a5-7a1ea6a5d2cf"
+		alt="my-photo-04"
+		width="800"
+	/>
+	<img
+		class="w-full max-w-[50rem] hover:grayscale-0"
+		src="https://github.com/h-alex2/images/assets/84281505/0935f7ff-ef0c-4ff4-8fe5-9a00c09a6f3d"
+		alt="my-photo-05"
+		width="800"
+	/>
+	<img
+		class="w-full max-w-[50rem] hover:grayscale-0"
+		src="https://github.com/h-alex2/images/assets/84281505/e97d29b1-eb19-4690-9743-7e61eb539129"
+		alt="my-photo-06"
+		width="800"
+	/>
+	<img
+		class="w-full max-w-[50rem] hover:grayscale-0"
+		src="https://github.com/h-alex2/images/assets/84281505/0ed685bd-e4c0-4a9e-a871-29eb0d0ad5d2"
+		alt="my-photo-08"
+		width="800"
+	/>
 </div>
