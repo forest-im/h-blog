fff0093fb6b2ba2bb7a912f7658a955c28257825
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-08-12 23:04:20 +0900

    Chore 레이아웃 변경

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,35 @@
 <script>
-	import "../app.css";
-	import "../prism.css";
+	import { page } from "$app/stores";
 	import Header from "$lib/components/Header.svelte";
 	import Footer from "$lib/components/Footer.svelte";
 	import ResponsiveLayer from "$lib/components/ResponsiveLayer.svelte";
+
+	import "../app.css";
+	import "../prism.css";
 </script>
 
 <body>
-	<div class="mx-auto flex min-h-[101vh] w-screen min-w-[350px] flex-col justify-center">
-		<main class="relative flex flex-1 flex-col">
-			<ResponsiveLayer>
-				<div class="max-w-[800px] max-[800px]:w-full min-[800px]:min-w-[800px]">
-					<Header />
-					<div class="min-h-[80px]" />
-					<div
-						on:click={() => window && window.scrollTo(0, 0)}
-						on:keydown={() => window && window.scrollTo(0, 0)}
-						class="fixed bottom-8 right-8 z-40 inline-flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-slate-300 opacity-90 shadow-md"
-					>
-						↑
+	{#if $page.route.id && $page.route.id.includes("/about")}
+		<slot />
+	{:else}
+		<div class="mx-auto flex min-h-[101vh] w-screen min-w-[350px] flex-col justify-center">
+			<main class="relative flex flex-1 flex-col">
+				<ResponsiveLayer>
+					<div class="max-w-[800px] max-[800px]:w-full min-[800px]:min-w-[800px]">
+						<Header />
+						<div class="min-h-[80px]" />
+						<div
+							on:click={() => window && window.scrollTo(0, 0)}
+							on:keydown={() => window && window.scrollTo(0, 0)}
+							class="fixed bottom-8 right-8 z-40 inline-flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-slate-300 opacity-90 shadow-md"
+						>
+							↑
+						</div>
+						<slot />
+						<Footer />
 					</div>
-					<slot />
-					<Footer />
-				</div>
-			</ResponsiveLayer>
-		</main>
-	</div>
+				</ResponsiveLayer>
+			</main>
+		</div>
+	{/if}
 </body>
