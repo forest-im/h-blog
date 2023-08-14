3a7a7cd0d7a501a33b276027ef92d8abf0fc26b5
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-08-14 16:12:29 +0900

    Feat email 전송 기능 변경

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,129 +1,32 @@
 <script>
-	import { onMount } from "svelte";
+	import emailjs from "@emailjs/browser";
 	import HomeSubLayout from "$lib/components/HomeSubLayout.svelte";
 	import HomeBackground from "$lib/components/HomeBackground.svelte";
 
-	export let data;
 	let copySuccess = false;
+	let sendResultMessage = null;
 
-	/**
-	 *
-	 * https://github.com/dwyl/learn-to-send-email-via-google-script-html-no-server/blob/master/form-submission-handler.js
-	 */
-
-	onMount(() => {
-		document.addEventListener("DOMContentLoaded", loaded, false);
-	});
-
-	function getFormData(form) {
-		var elements = form.elements;
-		var honeypot;
-
-		var fields = Object.keys(elements)
-			.filter(function (k) {
-				if (elements[k].name === "honeypot") {
-					honeypot = elements[k].value;
-					return false;
-				}
-				return true;
-			})
-			.map(function (k) {
-				if (elements[k].name !== undefined) {
-					return elements[k].name;
-					// special case for Edge's html collection
-				} else if (elements[k].length > 0) {
-					return elements[k].item(0).name;
-				}
-			})
-			.filter(function (item, pos, self) {
-				return self.indexOf(item) == pos && item;
-			});
-
-		var formData = {};
-		fields.forEach(function (name) {
-			var element = elements[name];
-
-			// singular form elements just have one value
-			formData[name] = element.value;
-
-			// when our element has multiple items, get their values
-			if (element.length) {
-				var data = [];
-				for (var i = 0; i < element.length; i++) {
-					var item = element.item(i);
-					if (item.checked || item.selected) {
-						data.push(item.value);
-					}
-				}
-				formData[name] = data.join(", ");
+	function sendEmail(e) {
+		emailjs.sendForm("service_49ayxzo", "template_t2z61th", e.target, "2s496EAakJS-xHIsC").then(
+			(result) => {
+				makeMessage("success");
+			},
+			(error) => {
+				makeMessage("fail");
 			}
-		});
-
-		// add form-specific values into the data
-		formData.formDataNameOrder = JSON.stringify(fields);
-		formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
-		formData.formGoogleSendEmail = form.dataset.email || ""; // no email by default
-
-		return { data: formData, honeypot: honeypot };
-	}
-	function handleSubmit(event) {
-		event.preventDefault(); // we are submitting via xhr below
-		var form = event.target;
-		var formData = getFormData(form);
-		var data = formData.data;
-
-		if (!data.name || !data.email || !data.message) {
-			alert("Please fill in the fields.");
-			return;
-		}
-
-		// If a honeypot field is filled, assume it was done so by a spam bot.
-		if (formData.honeypot) {
-			return false;
-		}
-
-		disableAllButtons(form);
-		var url = form.action;
-		var xhr = new XMLHttpRequest();
-		xhr.open("POST", url);
-		xhr.withCredentials = true;
-		xhr.setRequestHeader(
-			"Content-Type",
-			"application/x-www-form-urlencoded",
-			"Access-Control-Allow-Origin: https://www.im-alex.dev/contact"
 		);
-		xhr.onreadystatechange = function () {
-			if (xhr.readyState === 4) {
-				if (xhr.status === 200) {
-					form.reset();
-					alert("Form submitted successfully!");
-				} else {
-					alert("Form submission failed. Please try again.");
-				}
-			}
-		};
-		// url encode form data for sending as post data
-		var encoded = Object.keys(data)
-			.map(function (k) {
-				return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
-			})
-			.join("&");
-		xhr.send(encoded);
 	}
 
-	function loaded() {
-		// bind to the submit event of our form
-		var forms = document.querySelectorAll("form.gform");
-		for (var i = 0; i < forms.length; i++) {
-			forms[i].addEventListener("submit", handleSubmit, false);
+	async function makeMessage(mode) {
+		if (mode === "success") {
+			sendResultMessage = "메일이 성공적으로 전송되었습니다.";
+		} else {
+			sendResultMessage = "메일 발송이 실패하였습니다. 다시 시도해주세요.😭";
 		}
-	}
 
-	function disableAllButtons(form) {
-		var buttons = form.querySelectorAll("button");
-		for (var i = 0; i < buttons.length; i++) {
-			buttons[i].disabled = true;
-		}
+		setTimeout(() => {
+			sendResultMessage = null;
+		}, 10000);
 	}
 
 	async function copyToClipboard() {
@@ -159,9 +62,7 @@
 			<form
 				class="gform mt-5 flex h-full w-full flex-col gap-5"
 				data-email="glowhyun1@gmail.com"
-				method="POST"
-				action={data.emailUrl}
-				on:submit={handleSubmit}
+				on:submit|preventDefault={sendEmail}
 			>
 				<div>
 					<label for="name">Your Name</label>
@@ -175,11 +76,11 @@
 					<label for="message">Your Message</label>
 					<textarea name="message" class="h-[5rem] w-full text-black" id="message" />
 				</div>
-				<div class="text-red-600">현재 기능 버그로 수정중입니다.🥲</div>
-
-				<button
-					class="mt-5 block h-10 w-full cursor-not-allowed border border-white hover:text-blue-500"
-					type="submit">Send Message</button
+				{#if sendResultMessage}
+					<div class="mx-auto text-blue-500">{sendResultMessage}</div>
+				{/if}
+				<button class="mt-5 block h-10 w-full border border-white hover:text-blue-500" type="submit"
+					>Send Message</button
 				>
 			</form>
 		</div>
