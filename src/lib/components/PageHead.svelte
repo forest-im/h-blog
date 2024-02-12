10a9baa0a6246c170e3d96d6782566c59a7dde4e
Hazel <hazel@spoonradio.co>
2024-02-12 22:47:44 +0900

    Feat 트위터 메타 태그 수정

--- src/lib/components/PageHead.svelte
+++ src/lib/components/PageHead.svelte
@@ -17,9 +17,12 @@
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description ?? ""} />
 	<meta property="og:image" content={postImg ?? img} />
-	<meta name="twitter:card" content="summary" />
 	<meta name="twitter:title" content={title} />
-	<meta name="twitter:description" content={description} />
+	<meta name="twitter:description" content={description ?? ""} />
 	<meta name="twitter:image" content={postImg ?? img} />
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:url" content="https://www.im-alex.dev/" />
+	<meta name="twitter:app:name:iphone" content="Alex im" />
+	<meta name="twitter:app:name:googleplay" content="Alex im" />
 	<script src="https://cdn.jsdelivr.net/npm/prismjs@1/prism.min.js"></script>
 </svelte:head>
